dd61ac46ddeb8c1edbf743fc454c52ca60804c6a
jiangminsen <jiangminsen@h-partners.com>
2026-04-06 10:39:34 +0800

    接口补充部件裁剪的能力
    Signed-off-by: jiangminsen <jiangminsen@h-partners.com>

--- interfaces/kits/js/distributedBundle/distributed_bundle_unsupported.cpp
+++ interfaces/kits/js/distributedBundle/distributed_bundle_unsupported.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -29,6 +29,15 @@ napi_value GetRemoteAbilityInfo(napi_env env, napi_callback_info info)
     APP_LOGE("SystemCapability.BundleManager.DistributedBundleFramework not supported.");
     napi_value error = BusinessError::CreateCommonError(env, ERROR_SYSTEM_ABILITY_NOT_FOUND,
         "getRemoteAbilityInfo");
+    napi_throw(env, error);
+    return nullptr;
+}
+
+napi_value GetRemoteBundleVersionCode(napi_env env, napi_callback_info info)
+{
+    APP_LOGE("SystemCapability.BundleManager.DistributedBundleFramework not supported.");
+    napi_value error = BusinessError::CreateCommonError(env, ERROR_SYSTEM_ABILITY_NOT_FOUND,
+        "getRemoteBundleVersionCode");
     napi_throw(env, error);
     return nullptr;
 }
